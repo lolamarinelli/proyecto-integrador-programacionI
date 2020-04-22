9861583442bd3b037e00e2c10ef939f542ea6969
Paulina Ruiz <paulinaruiz@MacBook-Pro-de-Paulina.local>
2020-04-22 20:33:48 -0300

    agregue carousel de artistas

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosDelCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosDelCarousel, {
+        duration: 200
+      
+    });
+ });
+ 
  
