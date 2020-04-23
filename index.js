36ed2ba13da14bafb5e411d233e9d136a7902e7b
Paulina Ruiz <paulinaruiz@MacBook-Pro-de-Paulina.local>
2020-04-23 20:35:37 -0300

    no se q es

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
- 
- 
- 
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosDelCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosDelCarousel, {
+        duration: 200
+      
+    });
+ });
