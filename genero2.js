15341baf3d58b5783ad45a813be50e0db13a9cba
Paulina Ruiz <paulinaruiz@MacBook-Pro-de-Paulina.local>
2020-05-27 20:54:19 -0300

    nada solo pruebas fallidas en genero2.js

--- genero2.js
+++ genero2.js
@@ -2,8 +2,9 @@ window .addEventListener("load", function(){
     const apiRoute=' https://cors-anywhere.herokuapp.com/https://api.deezer.com/'
     fetch (apiRoute+' https://api.deezer.com/genre')
     .then(data=>data.json())
-    .then(repsuesta=>{
+    .then(respuesta=>{
+        
         console.log(respuesta);
     })
-}
-// esto es lo que esta en playground , pero nunca lo vimos asi nose bien dodne poner cada dato
+})
+// esto es lo que esta en playground , pero nunca lo vimos asi nose bien dodne poner cada dato.
