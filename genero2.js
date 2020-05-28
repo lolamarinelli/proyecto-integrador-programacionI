27657857cc4cb12a06ba20fc4e8d784812b27715
Paulina Ruiz <paulinaruiz@MacBook-Pro-de-Paulina.local>
2020-05-27 23:07:11 -0300

    pruebas js

--- genero2.js
+++ genero2.js
@@ -1,9 +1,15 @@
 window .addEventListener("load", function(){
-    const apiRoute=' https://cors-anywhere.herokuapp.com/https://api.deezer.com/'
-    fetch (apiRoute+' https://api.deezer.com/genre')
+    const apiRoute=' https://cors-anywhere.herokuapp.com/https://developers.deezer.com/api/explorer?url=genre/0'
+    fetch (apiRoute+' https://developers.deezer.com/api/explorer?url=genre/0')
     .then(data=>data.json())
-    .then(respuesta=>{
-        
+    .then(respuesta =>{
+        let genre= respuesta.data;
+        for(let index=0;index >respuesta.lenght;index++){
+            const apiroute= 'https://cors-anywhere.herokuapp.com/https://developers.deezer.com/api/explorer?url=genre/0'
+            const cadaGenero=genre[index];
+            let titulo= genre.type
+
+        }
         console.log(respuesta);
     })
 })
