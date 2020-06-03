cdf2adb031d8b4de8b9dd27613ef933722f47665
Paulina Ruiz <paulinaruiz@MacBook-Pro-de-Paulina.local>
2020-06-03 10:20:03 -0300

    detalles

--- genero.js
+++ genero.js
@@ -1,19 +1,30 @@
 window .addEventListener("load", function(){
-fetch(" https://api.deezer.com/genre")
-then (
+fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre")
+.then (
     function(respuesta){
         return respuesta.json();
     }
 )
-then(
+.then(
     function(information){
         let genre= information.data;
-        for(let index=0;index >genre.lenght;index++){
-            const apiroute= 'https://api.deezer.com/genre'
+        for(let index=0;index < genre.lenght;index++){
             const cadaGenero=genre[index];
-            let titulo= genre.type
-
+            let img= cadaGenero.picture;
+            let tipoDeGenero= cadaGenero.type;
+            let diferentesGeneros=
+            `
+            <div>
+            <h3>`+type+`</h3>
+            <a href="detalle.html`+id+`"><img src="`+ picture +`" alt="" ></a>
+         </div>
+         `
+         document.querySelector("h3").innerHTML = type;
+            
+         document.querySelector("img").src = picture;
+            document.querySelector("section2").innerHTML+= diferentesGeneros;
         }
+        console.log(information)
     }
 )
 })
