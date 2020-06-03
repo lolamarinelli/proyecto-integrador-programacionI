e231b7188823cddf062f5297d9803dd69f2c6ad1
Paulina Ruiz <paulinaruiz@MacBook-Pro-de-Paulina.local>
2020-06-03 20:16:42 -0300

    intentos de js en generos

--- genero.js
+++ genero.js
@@ -6,26 +6,23 @@ fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre")
     }
 )
 .then(
-    function(information){
-        let genre= information.data;
-        for(let index=0;index < genre.lenght;index++){
-            const cadaGenero=genre[index];
-            let img= cadaGenero.picture;
-            let tipoDeGenero= cadaGenero.type;
-            let diferentesGeneros=
-            `
-            <div>
-            <h3>`+type+`</h3>
-            <a href="detalle.html`+id+`"><img src="`+ picture +`" alt="" ></a>
-         </div>
-         `
-         document.querySelector("h3").innerHTML = type;
-            
-         document.querySelector("img").src = picture;
-            document.querySelector("section2").innerHTML+= diferentesGeneros;
+    function(informacion){
+        let genero= informacion.data;
+        for (let index=0;index< genero.lenght;index++){
+            cadaGenero= genero[index];
+            let img=genero.data.picture;
+            let tipoDeGenero= genero.data.name;
+       
+            let generos=
+             `<section class="section2">
+                <h4>`+ tipoDeGenero +`</h4>
+                <img src="`+ img +`" alt="">
+             </section>
+             `
+      document.querySelector(".section2").innerHTML += generos 
+      
         }
-        console.log(information)
-    }
+        console.log(informacion);
+    } 
 )
 })
-// no se que onda donde va el endpoint y ddonde encuento lo que usabamos con gyphy
