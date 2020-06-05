492d6b9414472ab00867696db97450b82482bce8
Paulina Ruiz <paulinaruiz@MacBook-Pro-de-Paulina.local>
2020-06-05 00:01:16 -0300

    mas oruebas , todo ok

--- artista.js
+++ artista.js
@@ -1,1 +1,14 @@
-window .addEventListener("load", function(){}
+window .addEventListener("load", function(){
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27")
+    .then (
+        function(respuesta){
+            return respuesta.json();
+        }
+    )
+    .then(
+        function(informacion){
+            for(let index=0;index< genero.lenght;index++){
+                let detalleDeArtista=
+            }
+        })
+}
--- genero.js
+++ genero.js
@@ -8,18 +8,19 @@ fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre")
 .then(
     function(informacion){
         let genero= informacion.data;
+        console.log(genero)
         for (let index=0;index< genero.lenght;index++){
             cadaGenero= genero[index];
             let img=genero.data.picture;
             let tipoDeGenero= genero.data.name;
        
             let generos=
-             `<section class="section2">
+             `
                 <h4>`+ tipoDeGenero +`</h4>
                 <img src="`+ img +`" alt="">
-             </section>
-             `
-      document.querySelector(".section2").innerHTML += generos 
+             
+             `;
+      document.querySelector(".section2").innerHTML + generos 
       
         }
         console.log(informacion);
