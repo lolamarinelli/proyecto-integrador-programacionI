bd4b86431cbc7a10157e85e763da0ab050696975
Paulina Ruiz <paulinaruiz@MacBook-Pro-de-Paulina.local>
2020-06-17 20:13:27 -0300

    hicimos java adetalle de artista

--- artista.js
+++ artista.js
@@ -39,9 +39,36 @@ window .addEventListener("load", function(){
         }
     )
     .then(
-        function(informacion){
-            let detalleArtista=informacion;
-            console.log(informacion)
+        function(info){
+            let detalleLista=info.data;
+            console.log(info)
+            for (let index=0;index< detalleLista.length;index++){
+                cadaRenglon= detalleLista [index];
+            let canciones=cadaRenglon.title
+            let imagenDeAlbum=cadaRenglon.album.cover;
+            let nombreDeAlbum=cadaRenglon.album.title;
+            let listadoDeCanciones=
+            `
+            <li class="renglones">
+                            <div class="listaTracks"> 
+                              <div> <p>`+ (index+1) +`</p></div>
+                              <div> <section class="dentrolista"><h4 class="titulos">`+canciones+`</h4> </div>
+                              <div> <span class="uk-margin-small-right" uk-icon="check"></span></div>
+
+                              <div> <a href="" uk-icon="heart" class="uk-margin-small-right"></a></div>
+                            </div>
+            </li>
+            `
+            document.querySelector(".canciones1").innerHTML+= listadoDeCanciones;
             
-        })
+            listaDeAlbums=
+            `
+            <div class="1">
+                <a href="./album.html">
+                    <img class="foto" src="`+imagenDeAlbum+`" alt=""><p class="titulo">`+nombreDeAlbum+`</p>
+                </a>
+            </div> 
+            `
+            document.querySelector(".albums").innerHTML+= listaDeAlbums;
+        }})
 })
