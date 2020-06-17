1ebc04c6e57907d9c21971c9a38ca14cb255e283
Lola Marinelli <lolamarinelli@MacBook-Pro-de-Lola.local>
2020-06-17 18:51:35 -0300

    artistas linkeado

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
 window .addEventListener("load", function(){
-    fetch(" https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/27")
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
     .then(
         function(respuesta){
             return respuesta.json();
         }
     )
     .then(function(info){
-        let artista=info.artist;
-        console.log(info)
-            cadaArtista=artista [index];
-
-            let img=cadaArtista.picture;
-            let foto= img;
-        document.querySelector(".slider1").innerHTML += foto;
+        console.log(info);
+        let artistas = info.artists.data
+        for (let index = 0; index < artistas.length; index++) {
+            const cadaArtista = artistas[index];
+            console.log(cadaArtista);
+            let nombre = cadaArtista.name;
+            let id = cadaArtista.id
+            let img = cadaArtista.picture
+            
+            let nuevoHtml= `
+            <li>
+                        <img src="`+ img +`" alt="" class="slider1">
+                        <div class="uk-position-center uk-panel"><h1>
+                        <a href= "Artistas.html?pepinito=`+ id +`">`+ nombre +`</a>
+                        </h1></div>
+            </li>
+            `
+            document.querySelector(".topartistas").innerHTML += nuevoHtml
+        }
+        
             
         
     })
 
-<<<<<<< HEAD
-
-
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127")
-    .then(
-        function(respuesta){
-            return respuesta.json();
-        }
-    )
-    .then(
-        function(informacion){
-            let album=informacion;
-           ;
-        }
-
-    )
-})
-
-=======
     
 })
->>>>>>> d0e0e062adde9ea4bcb67c9f9bc6327ed469461d
